f5efec8fffff075745a00060e94c5f1267a01be4
ChrisCockram <69448062+ChrisCockram@users.noreply.github.com>
2021-12-16 08:31:27 +0000

    V Update 30

--- api/[qr].js
+++ api/[qr].js
@@ -5,11 +5,18 @@ if (
       `${process.env.PWD}/node_modules/canvas/build/Release:`,
     )
   ) {
-    console.log('ADDING LD_LIBRARY_PATH')
     process.env.LD_LIBRARY_PATH = `${
       process.env.PWD
     }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
   }
+
+
+
+
+module.exports = (req, res) => {
+    res.send("Hello");
+};
+
 
 /*
 const QRCode = require('easyqrcodejs-nodejs');

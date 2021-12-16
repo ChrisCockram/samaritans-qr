2694b73e098a21f6db5a1b74d871f8ae1f09742f
ChrisCockram <69448062+ChrisCockram@users.noreply.github.com>
2021-12-16 15:05:24 +0000

    61

--- api/[qr].js
+++ api/[qr].js
@@ -1,4 +1,23 @@
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+if (
+    process.env.LD_LIBRARY_PATH == null ||
+    !process.env.LD_LIBRARY_PATH.includes(
+      `${process.env.PWD}/node_modules/canvas/build/Release:`,
+    )
+  ) {
+    console.log('ADDING LIBRARY')
+    process.env.LD_LIBRARY_PATH = `${
+      process.env.PWD
+    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+
+    console.log(process.env.LD_LIBRARY_PATH);
+  }
+
+
+
+
 const { Canvas, Image } = require('canvas');
+
 
 module.exports = (req, res) => {  
     res.send('Ahhh');

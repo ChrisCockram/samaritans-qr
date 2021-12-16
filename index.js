894d28e896baceab689afcdbb7342eb0756c582f
Chris Cockram <chris.cockram@gmail.com>
2021-12-16 00:28:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const QRCode = require('easyqrcodejs-nodejs');
+const mergeImages = require('merge-images');
+const { Canvas, Image } = require('canvas');
+const { v4: uuidv4 } = require('uuid');
+const express = require('express');
+
+
+const app = express()
+const port = 3000
+
+app.get('/qr/', async (req, res) => {
+    if (req.query.b64url == undefined) {
+        req.query.b64url = ''
+    }
+
+    let buff = new Buffer(req.query.b64url, 'base64');
+    let url = buff.toString('ascii');
+
+    generateQr(url).then(b64=>{
+        let json={'b64':b64};
+        res.send(json)
+    });
+
+})
+app.use(express.static('public'))
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+function generateQr(url,bg='donate'){
+    return new Promise((resolve, reject) => {
+        let uid = uuidv4();
+        let tempFile = './temp/'+uid+'_temp.png'
+        let w = 1000;
+        // Options
+        let options = {
+            width: w,
+            height: w,
+            text: url,
+            colorDark : "#8bbe29",
+            colorLight : "#115e67",
+            logo: "logo.png",
+            logoWidth: 340,
+            logoHeight: 340,
+            correctLevel : QRCode.CorrectLevel.H,
+            quietZone: 0,
+            quietZoneColor: "rgba(0,0,0,0)",
+
+        };
+
+        // New instance with options
+        let qrcode = new QRCode(options);
+
+        // Save QRCode image
+        qrcode.saveImage({
+            path: tempFile
+        }).then(data=>{
+            mergeImages([
+                { src: './bg/donate.png', x: 0, y: 0 },
+                { src: tempFile, x: 40, y: 40 }
+            ], {
+                Canvas: Canvas,
+                Image: Image
+            }).then(b64=>{
+                    require("fs").unlink(tempFile,()=>{console.log('deleted')});
+                    b64 = b64.replace(/^data:image\/png;base64,/, "");
+                    return resolve(b64)
+                }
+            );
+        });
+    });
+
+}
+
+
+
+

0762d3331ef9042053e1c4badad95a9fa806d126
ChrisCockram <69448062+ChrisCockram@users.noreply.github.com>
2021-12-16 08:26:05 +0000

    V Update 28

--- api/[qr].js
+++ api/[qr].js
@@ -1,15 +1,15 @@
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
 if (
     process.env.LD_LIBRARY_PATH == null ||
     !process.env.LD_LIBRARY_PATH.includes(
       `${process.env.PWD}/node_modules/canvas/build/Release:`,
     )
   ) {
+    console.log('ADDING LD_LIBRARY_PATH')
     process.env.LD_LIBRARY_PATH = `${
       process.env.PWD
     }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
   }
-
-
 
 const QRCode = require('easyqrcodejs-nodejs');
 const mergeImages = require('merge-images');
